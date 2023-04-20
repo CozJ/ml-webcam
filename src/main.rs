f06b8b13c31d2e8b05152fdc66b34b5ca73aae69
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-04-20 03:01:57 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::time::Duration;
 
-use opencv::core::{Size, Scalar, Point, Mat, find_file, rotate, ROTATE_180};
+use opencv::core::{find_file, rotate, Mat, Point, Scalar, Size, ROTATE_180};
 use opencv::highgui::{imshow, named_window, wait_key};
 use opencv::imgproc::ellipse;
-use opencv::objdetect::{CascadeClassifier};
+use opencv::objdetect::CascadeClassifier;
 use opencv::prelude::*;
-use opencv::videoio::VideoCaptureTrait;
 use opencv::types::VectorOfRect;
+use opencv::videoio::VideoCaptureTrait;
 use serial::prelude::*;
+use serial::unix::TTYPort;
 
 fn main() {
-
-    let mut port = serial::open("/dev/ttyACM0").unwrap();
+    let mut port = serial::open("/dev/tty.usbmodem11301").unwrap();
     port.reconfigure(&|settings| {
         settings.set_baud_rate(serial::Baud9600)?;
+        settings.set_char_size(serial::Bits8);
+        settings.set_parity(serial::ParityNone);
+        settings.set_stop_bits(serial::Stop1);
+        settings.set_flow_control(serial::FlowNone);
         Ok(())
-    }).unwrap();
-    port.set_timeout(Duration::from_millis(1000)).unwrap();
-
+    })
+    .unwrap();
+    port.set_timeout(Duration::from_millis(10000)).unwrap();
 
     let window = "Capture - Face detection";
     let xml = find_file("haarcascades/haarcascade_frontalface_alt.xml", true, false).unwrap();
@@ -38,7 +42,27 @@ fn main() {
         if wait_key(10).unwrap() == 27 {
             break;
         }
-        let data_to_send = "left/n".as_bytes();
+    }
+}
+
+fn commands(command: String, port: &mut TTYPort) {
+    if command == "left" {
+        let data_to_send = "left\n".as_bytes();
+        port.write_all(data_to_send).unwrap();
+        println!("Sent: {:?}", data_to_send);
+    }
+    if command == "right" {
+        let data_to_send = "right\n".as_bytes();
+        port.write_all(data_to_send).unwrap();
+        println!("Sent: {:?}", data_to_send);
+    }
+    if command == "up" {
+        let data_to_send = "up\n".as_bytes();
+        port.write_all(data_to_send).unwrap();
+        println!("Sent: {:?}", data_to_send);
+    }
+    if command == "down" {
+        let data_to_send = "down\n".as_bytes();
         port.write_all(data_to_send).unwrap();
         println!("Sent: {:?}", data_to_send);
     }
@@ -50,10 +74,32 @@ fn detect_and_display(frame: &mut Mat, face_cascade: &mut CascadeClassifier) {
     let mut frame_gray_eq = Mat::default();
     opencv::imgproc::equalize_hist(&frame_gray, &mut frame_gray_eq).unwrap();
     let mut faces = VectorOfRect::new();
-    face_cascade.detect_multi_scale(&frame_gray_eq, &mut faces, 1.1, 3, 0, Size::new(30, 30), Size::new(0, 0)).unwrap();
+    face_cascade
+        .detect_multi_scale(
+            &frame_gray_eq,
+            &mut faces,
+            1.1,
+            3,
+            0,
+            Size::new(30, 30),
+            Size::new(0, 0),
+        )
+        .unwrap();
     for face in faces.iter() {
         let center = Point::new(face.x + face.width / 2, face.y + face.height / 2);
         let axes = Size::new(face.width / 2, face.height / 2);
-        ellipse(frame, center, axes, 0.0, 0.0, 360.0, Scalar::new(255.0, 0.0, 255.0, 0.0), 4, opencv::imgproc::LINE_8, 0).unwrap();
+        ellipse(
+            frame,
+            center,
+            axes,
+            0.0,
+            0.0,
+            360.0,
+            Scalar::new(255.0, 0.0, 255.0, 0.0),
+            4,
+            opencv::imgproc::LINE_8,
+            0,
+        )
+        .unwrap();
     }
 }
